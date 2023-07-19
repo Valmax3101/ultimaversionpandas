568eaff98a774250401bb53db775033104865eb3
Andres <felipe24he@hotmail.com>
2023-07-19 17:48:03 -0500

    ok

---
+++ data/clientes.py
@@ -0,0 +1,23 @@
+cliente1=["Valentina Osorio","05/07/2023","150000"]
+cliente2=["Alejandro Martinez","09/03/2020","95000"]
+cliente3=["Camila Pareja","15/11/2018","100000"]
+cliente4=["Manuel Valencia","01/08/2021","200000"]
+cliente5=["Fabian Lopez","25/04/2021","80000"]
+cliente6=["Manuela Álvarez","05/12/2022","75500"]
+cliente7=["Angel Velez","28/10/2020","120000"]
+cliente8=["Angela Nanclares","30/08/2019","90000"]
+cliente9=["Maximiliano Maya","31/10/2020","105000"]
+cliente10=["Sebastian Maya","16/06/2021","170000"]
+
+clientes=[
+    cliente1,
+    cliente2,
+    cliente3,
+    cliente4,
+    cliente5,
+    cliente6,
+    cliente7,
+    cliente8,
+    cliente9,
+    cliente10
+]
---
+++ data/proveedores.py
@@ -0,0 +1,43 @@
+proveedor1=["Zenu carnicos alimenticicos", "31-05-2023",580000,-250000]
+proveedor2=["Coca-cola","10-10-2020",1200000,-80000]
+proveedor3=["Colanta","14-08-2000",863000,-63000]
+proveedor4=["Alpina","05-12-1998",520000,-2000]
+proveedor5=["Bavaria","28-02-2010",180000,-70000]
+proveedor6=["Postobón","03-01-2018",1800000,-0]
+proveedor7=["Grupo Nutresa","16-01-2005",500000,-35000]
+proveedor8=["Kopps Commercial","23-09-2020",742000,-21500]
+proveedor9=["Colombina","23-09-2020",760000,-87000]
+proveedor10=["Gaseosas Lux","28-07-1996",950000,-12000]
+proveedor11=["Industria Alimenticia Chorisabor Sas","19-11-2022",2500000,-1000]
+proveedor12=["Aguardiente Antioqueño","27-06-2006",1300000,-38600]
+proveedor13=["Museo de la Uva y el Vino Grajales","11-11-1988",300000,-250000]
+proveedor14=["CremHelado","03-12-2011",410000,-74000]
+proveedor15=["Productos Ramo","14-06-2003",1800000,-41200]
+proveedor16=["Haz de Oro","23-10-2015",835600,-69400]
+proveedor17=["Alquería","10-03-2017",230000,-30000]
+proveedor18=["Roa","24-06-2008",340000,-60000]
+proveedor19=["Sello rojo","25-07-2018",90000,-60000]
+proveedor20=["Quaker","19-05-2015",640000,-80000]
+
+proveedores=[
+    proveedor1,
+    proveedor2,
+    proveedor3,
+    proveedor4,
+    proveedor5,
+    proveedor6,
+    proveedor7,
+    proveedor8,
+    proveedor9,
+    proveedor10,
+    proveedor11,
+    proveedor12,
+    proveedor13,
+    proveedor14,
+    proveedor15,
+    proveedor16,
+    proveedor17,
+    proveedor18,
+    proveedor19,
+    proveedor20
+    ]
